4c3f04a0aa45e48404d792f9be5732fd6498530b
onlysilvercrow <sahilpatel1906@gmail.com>
2024-06-10 17:50:44 -0500

    Dynamically generate timeline list on index

--- src/javascript/shared.js
+++ src/javascript/shared.js
@@ -24,12 +24,30 @@ const TopProjects = [
     
 ]
 
+// Timeline information
+const timeline = [
+    {
+        "year": "2000",
+        "info": "Birth",
+        "side": "left"
+    },
+    {
+        "year": "2018-2022",
+        "info": "Started University",
+        "side": "right"
+    },
+    {
+        "year": "2022-2023",
+        "info": "F1 Website Data Graphing & Analysis",
+        "side": "left"
+    }
+    
+]
 
 // function to create a list of projects based on provided json and elementId
 function createProjectsList(projectsList, elemId) {
     for (let project of projectsList) {
         $(elemId).append(`
-            
              <article>
                 <h4>${project.name}</h4>
                 <a href="${project.github}" /><img src="" alt="Github Link to project ${project.github}"></a>
@@ -39,14 +57,30 @@ function createProjectsList(projectsList, elemId) {
 
                 </div>
             </article>
-        
-        
-        
         `);
-        
     }
 }
 
+
+// function to create a timeline components
+function createTimeline(timelineItems, elemId) {
+    for (let item of timelineItems) {
+        $(elemId).append(`          
+             <article class="container ${item.side}">
+                <div class="content">
+                    <h4>${item.year}</h4>
+                    <p>${item.info}</p>
+                </div>
+            </article>  
+        `);     
+    }
+}
+
+// Generate top projects list
 createProjectsList(TopProjects, '#recent-projects')
+
+
+// Generate timeline components
+createTimeline(timeline, '#timeline-div')
 
 
