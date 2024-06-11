b4f4e45542d177c36bb2fb65c1f67fd229f76c29
onlysilvercrow <sahilpatel1906@gmail.com>
2024-06-11 10:35:36 -0500

    Add email functionality

---
+++ src/javascript/contact.js
@@ -0,0 +1,16 @@
+function sendEmail(senderName, email, subject, message) {
+    var params = {
+        subject: subject,
+        email: email,
+        from_name: senderName,
+        message: message
+    }
+    emailjs.send("service_k1q1xpl", "template_hvirplg", params).then(function (res){
+        alert("Sent Mail!");
+    })
+};
+
+$("#contact-form").on("submit", function (event) {
+    event.preventDefault();
+    sendEmail(event.target[0].value, event.target[1].value, event.target[2].value, event.target[3].value);
+});
